bb15b6d6c820c2fe22fbb31cb4e28590e7304658
WhenDustSettles <54006172+WhenDustSettles@users.noreply.github.com>
2020-07-11 16:53:37 +0530

    Script with Duration and Time of Day Slider
    Additional code for merging with other features will be added later.

--- dash-implementation/dash-script.py
+++ dash-implementation/dash-script.py
@@ -12,8 +12,11 @@ from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output
 from datetime import datetime as dt
+
+import socket
+print(socket.gethostname())
 
 # Stylesheet
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -21,24 +24,20 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 
 # Load  Data
-df = pd.read_csv("./data.csv")
+df =pd.read_csv("data.csv")
 
 #Color scale of edges
 viridis = cm.get_cmap('viridis', 12)
 # Define Color
 df['Dura_color']=(df['Duration']/df['Duration'].max()).apply(viridis)
-
-#Node calculation and dataframe cleaning
-
-nodes=list(np.union1d(df['Caller'].unique(),df['Receiver'].unique()))
 df['Date']=df['Date'].apply(pd.to_datetime).dt.date
-df['Caller_node']=df['Caller'].apply(lambda x:list(nodes).index(x))
-df['Receiver_node']=df['Receiver'].apply(lambda x:list(nodes).index(x))
 
 #### Plots
 # Plot Graph of calls
 def plot_network(df):
     nodes=np.union1d(df['Caller'].unique(),df['Receiver'].unique())
+    print(nodes.shape[0])
+
     G = nx.random_geometric_graph(nodes.shape[0], 0.125)
     df['Caller_node']=df['Caller'].apply(lambda x:list(nodes).index(x))
     df['Receiver_node']=df['Receiver'].apply(lambda x:list(nodes).index(x))
@@ -49,7 +48,7 @@ def plot_network(df):
         x1,y1=G.nodes[x['Receiver_node']]['pos']
         edge_trace.append(dict(type='scatter',
         x=[x0,x1], y=[y0,y1],
-        line=dict(width=0.5, color='rgba'+str(x['Dura_color']).replace(']',')').replace('[','(')),
+        line=dict(width=0.5, color='rgba'+str(x['Dura_color'])),
         hoverinfo='none',
         mode='lines'))
     df.apply(add_coords,axis=1)
@@ -84,7 +83,35 @@ def plot_network(df):
     fig.update_layout(transition_duration=500)
     return fig
 
-# Create an image for the same.
+
+
+
+
+
+
+
+#Apps default variables
+default_duration_slider_val = [0,10]
+default_time_slider_val = ['00:00', '24:00']
+
+
+#Loop to generate marks for Time
+time_str = ['0','0',':','0','0']
+times = {0:"".join(time_str)}
+for i in range(0,48):
+    if i%2 == 0:
+        time_str[3] = '3'
+    else:
+        time_str = list( str(int("".join(time_str[0:2]) ) + 1 ).zfill(2) + str(':00') )
+    times[i+1] = "".join(time_str)
+
+
+
+
+
+
+
+
 # Layout of App
 app.layout = html.Div(children=[
     html.H1(children='CDR Analyser'),
@@ -95,7 +122,6 @@ app.layout = html.Div(children=[
     html.Div(
         id='date-selected'
     ),
-    html.Div(id='message'),
     dcc.Graph(
         id='network-plot',
         figure=plot_network(df[df['Date']==df['Date'].min()].reset_index(drop=True))
@@ -107,52 +133,110 @@ app.layout = html.Div(children=[
         initial_visible_month=dt(2020, 6, 5),
         date=str(dt(2020, 6, 5, 0, 0, 0 )) 
     ),
-    dcc.Dropdown(
-        id='caller-dropdown',
-        options =[{'label':'None','value':''}]+ [{'label': k, 'value': k} for k in df['Caller'].unique()],
-        value='',
+    
+    
+    dcc.RangeSlider(
+        id='duration-slider',
+        min=0,
+        max=df['Duration'].max(),
+        step=1,
+        value=default_duration_slider_val,
+        dots=True,
+        
     ),
-    html.Div(
-        id='filtered-data',
-        style={'display': 'none'}
-    )
+    
+    dcc.RangeSlider(
+        id='time-slider',
+        min=0,
+        max= 48,
+        step=None,
+        marks=times,
+        dots=True,
+        value = [0,48],
+        pushable=1
+        
+    ),
+    
+    
+
+    
+    html.Div(id='duration-slider-output-container', children="Duration selected : {} to {} units".format(default_duration_slider_val[0], default_duration_slider_val[1])),
+
+    html.Div(id='time-slider-output-container', children="Time of Day selected : {} to {} hours".format(default_time_slider_val[0], default_time_slider_val[1]))
+    
+    
 ])
 
 # Callbacks
-# Callback to update df used for plotting
-# Callback to filter dataframe
 @app.callback(
-    [Output(component_id='filtered-data', component_property='children'),Output(component_id='message',component_property='children')],
-    [Input(component_id='date-picker', component_property='date'),Input(component_id='caller-dropdown', component_property='value')]
+    Output(component_id='network-plot', component_property='figure'),
+    
+    [Input(component_id='date-picker', component_property='date'), Input(component_id='duration-slider', component_property='value'), Input(component_id='time-slider', component_property='value')]
 )
-def update_filtered_div(selected_date,selected_number):
-    if selected_number!='':
-        filtered_df=df[(df['Date']==pd.to_datetime(selected_date))&(df['Caller']==int(selected_number))].reset_index(drop=True)
-    else:
-        filtered_df=df[df['Date']==pd.to_datetime(selected_date)]
-    if filtered_df.shape[0]==0:
-        # Update this 
-        return dash.no_update,'Nothing Matches that Query'      
-    else:
-        # Update Filtered Dataframe
-        return filtered_df.to_json(date_format='iso',orient='split'),'Updated'
-# Callback to update network plot
-@app.callback(
-    Output(component_id='network-plot',component_property='figure'),
-    [Input(component_id='filtered-data',component_property='children')]
-)
-def update_network_plot(filtered_data):
-    return plot_network(pd.read_json(filtered_data, orient='split'))
+def update_output_div(selected_date, selected_duration, selected_time):
+    """
+    This callback is used to update:
+    1) The main graph by changing it's linked dataframe by 'date' and 'Duration' currently selected.
+    """
+    
+    print('Callback Called')
 
-# Callback to change selectors including caller no. according to date
+    print('selected_date', selected_date)
+    print('selected_duration', selected_duration)
+    print('selected_time', selected_time)
+    # ^^^^ For debugging purposes only 
+    
+    
+    #Handling Date
+    date_filt_df=df[df['Date']==pd.to_datetime(selected_date)].reset_index(drop=True)
+    
+    #Handling Duration of call
+    dur_date_filt_df = date_filt_df[ (date_filt_df['Duration'] >= selected_duration[0]) & (date_filt_df['Duration'] <= selected_duration[1]) ]
+    
+    #Handling Time of Day
+    time_dur_date_filt_df = dur_date_filt_df[ (dur_date_filt_df['Time'] < times[selected_time[1]]) & (dur_date_filt_df['Time'] >= times[selected_time[0]]) ]
+    
+    return plot_network(time_dur_date_filt_df)
+
+
+
+
+
 @app.callback(
-    Output(component_id='caller-dropdown',component_property='options'),
-    [Input(component_id='date-picker', component_property='date')]
+    Output(component_id='duration-slider-output-container', component_property='children'),
+    
+    [Input(component_id='duration-slider', component_property='value')]    
 )
-def update_phone_div(selected_date):
-    return [{'label':'None','value':''}]+[{'label': k, 'value': k} for k in df[df['Date']==pd.to_datetime(selected_date)]['Caller'].unique()]
+def update_selected_duration(selected_duration):
+    """
+    This callback is used to update:
+    1) The string which denotes the currently selected duration. 
+    2) The string which denotes the currently selected time
+    """
+    return "Duration selected : {} to {} units".format(selected_duration[0], selected_duration[1])
+
+
+
+@app.callback(
+    Output(component_id='time-slider-output-container', component_property='children'),
+    
+    [Input(component_id='time-slider', component_property='value')]    
+)
+def update_selected_duration(selected_time):
+    """
+    This callback is used to update:
+    1) The string which denotes the currently selected time.
+    """
+    return "Time of Day selected : {} to {} hours".format(times[selected_time[0]], times[selected_time[1]])
+
+
+
+
+
+
+
 
 # Run Server
 if __name__ == '__main__':
-    app.run_server(debug=True,port=8000)
-
+    #app.run_server(debug=True, port=8000)
+    app.run_server(debug = True,host='0.0.0.0')
