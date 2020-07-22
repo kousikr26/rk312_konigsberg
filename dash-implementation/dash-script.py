5d76934d84d9d439fac7bbe7fe6cc5f3234ba0bf
kousikr26 <kousik18@iitg.ac.in>
2020-07-22 14:27:35 +0530

    hover and select

--- dash-implementation/dash-script.py
+++ dash-implementation/dash-script.py
@@ -21,8 +21,14 @@ import pygraphviz as pgv
 # Stylesheet
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-
+styles = {
+    'pre': {
+        'border': 'thin lightgrey solid',
+        'overflowX': 'scroll'
+    }
+}
+coords_to_node={} # Dictionary that stores coordinates to node number
+data_columns=["Caller","Receiver","Date","Time","Duration","TowerID","IMEI"]
 # Load  Data
 df = pd.read_csv("../data/data.csv")
 
@@ -57,6 +63,8 @@ def plot_network(df):
     def add_coords(x): 
         x0,y0=pos[x['Caller_node']]
         x1,y1=pos[x['Receiver_node']]
+        coords_to_node[(x0,y0)]=x['Caller_node']
+        coords_to_node[(x1,y1)]=x['Receiver_node']
         edge_trace.append(dict(type='scatter',
         x=[x0,x1], y=[y0,y1],
         line=dict(width=0.5, color='rgba'+str(x['Dura_color']).replace(']',')').replace('[','(')),
@@ -92,6 +100,9 @@ def plot_network(df):
                     yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
                     )
     fig.update_layout(transition_duration=500)
+    fig.update_layout(clickmode='event+select')
+
+    fig.update_traces(marker_size=20)
     return fig
 
 # Create an image for the same.
@@ -110,6 +121,41 @@ app.layout = html.Div(children=[
         id='network-plot',
         figure=plot_network(df[df['Date']==df['Date'].min()].reset_index(drop=True))
     ),
+    html.Div(className='row', children=[
+        html.Div([
+            dcc.Markdown("""
+                **Hover Data**
+
+                Mouse over values in the graph.
+            """),
+            html.Pre(id='hover-data', style=styles['pre'])
+        ], className='three columns'),
+
+        html.Div([
+            dcc.Markdown("""
+                **Click Data**
+
+                Click on points in the graph.
+            """),
+            html.Pre(id='click-data', style=styles['pre']),
+        ], className='three columns'),
+
+        html.Div([
+            dcc.Markdown("""
+                **Selection Data**
+
+                Choose the lasso or rectangle tool in the graph's menu
+                bar and then select points in the graph.
+
+                Note that if `layout.clickmode = 'event+select'`, selection data also 
+                accumulates (or un-accumulates) selected data if you hold down the shift
+                button while clicking.
+            """),
+            html.Pre(id='selected-data', style=styles['pre']),
+        ], className='three columns')
+
+       
+    ]),
     dcc.DatePickerSingle(
         id='date-picker',
         min_date_allowed=df['Date'].min(),
@@ -137,6 +183,11 @@ app.layout = html.Div(children=[
         options =[{'label':'None','value':''}]+ [{'label': k, 'value': k} for k in df['Receiver'].unique()],
         value='',
     ),
+    html.Label('View selected'),
+    dcc.Input(
+        id='selected-view',
+        placeholder='Enter comma separated phone numbers',
+        value='', type='text'),
 ])
 
 # Callbacks
@@ -144,9 +195,12 @@ app.layout = html.Div(children=[
 # Callback to filter dataframe
 @app.callback(
     [Output(component_id='filtered-data', component_property='children'),Output(component_id='message',component_property='children')],
-    [Input(component_id='date-picker', component_property='date'),Input(component_id='select-caller-receiver',component_property='value'),Input(component_id='caller-dropdown', component_property='value'),Input(component_id='receiver-dropdown', component_property='value')]
+    [Input(component_id='date-picker', component_property='date'),Input(component_id='select-caller-receiver',component_property='value'),Input(component_id='caller-dropdown', component_property='value'),Input(component_id='receiver-dropdown', component_property='value'),Input(component_id='selected-view', component_property='value')]
 )
-def update_filtered_div_caller(selected_date,selected_option,selected_caller,selected_receiver):
+def update_filtered_div_caller(selected_date,selected_option,selected_caller,selected_receiver,selected_users):
+    
+    user_list=list(map(str.strip,list(selected_users.split(',')))) # Use this list of users to find connected component in text entry mode (TODO #4)
+ 
     if(selected_option==1):        
         if selected_caller!='':
             filtered_df=df[(df['Date']==pd.to_datetime(selected_date))&(df['Caller']==int(selected_caller))].reset_index(drop=True)
@@ -169,6 +223,35 @@ def update_filtered_div_caller(selected_date,selected_option,selected_caller,sel
         else:
             # Update Filtered Dataframe
             return filtered_df.to_json(date_format='iso',orient='split'),'Updated'
+@app.callback(
+    Output('hover-data', 'children'),
+    [Input('network-plot', 'hoverData')])
+def display_hover_data(hoverData):
+    #TODO #1 similar to displlay_click_data find node and find corresponding statistics and return a string containing them
+    return "Hover data..."
+
+
+@app.callback(
+    Output('click-data', 'children'),
+    [Input('network-plot', 'clickData')])
+def display_click_data(clickData):
+    if clickData is not None:
+        nodeNumber=coords_to_node[(clickData['points'][0]['x'],clickData['points'][0]['y'])]
+        return df[(df['Caller_node']==nodeNumber) |(df['Receiver_node']==nodeNumber)][data_columns].to_string(index=False)
+    return "CLick on a node to view more data"
+    
+
+
+@app.callback(
+    Output('selected-data', 'children'),
+    [Input('network-plot', 'selectedData')])
+def display_selected_data(selectedData):
+    # TODO #2 This is for multi select run multisource bfs and give all nodes in component in return string
+    # TODO #3 Graph should also be filtered and only nodes in component should be displayed
+    print(selectedData)
+    return json.dumps(selectedData, indent=2)
+
+
 # Callback to update network plot
 @app.callback(
     Output(component_id='network-plot',component_property='figure'),
--- dash-implementation/stats.py
+++ dash-implementation/stats.py
@@ -13,20 +13,12 @@ from collections import defaultdict
 # Reading the csv into a Pandas dataframe df
 
 df = pd.read_csv("data.csv")
-rows_count=len(df.index)
+rows_count = len(df.index)
 
 #returns the mean duration of calls
 def meanDurationOfCalls(x):
-    r = 0
-    t = 0
-    j = 0
-    for i in range(rows_count):
-        if df.at[i,'Caller'] == x or df.at[i,'Receiver'] == x:
-            t = df.at[i,'Duration']/(j+1)
-            r = r * j
-            r = r / (j+1)
-            r = r + t
-            j = j + 1
+    r = df[df.Caller == x].duration.mean()
+    r = r+df[df.Reciever == x].duration.mean()
     return r
 
 #returns a dictionary with the number of minutes of talk
@@ -36,26 +28,26 @@ def meanDurationOfCalls(x):
 def peakHours(x):
     m = {}
     for i in range(24):
-        m[i]=0
-    for i in range(rows_count):
-        if df.at[i,'Caller'] == x or df.at[i,'Receiver'] == x:
-            h = int(df.at[i,'Time'][0:2])
-            min = int(df.at[i,'Time'][3:5])
-            dur = df.at[i,'Duration']
-            min = 60-min
-            if(dur>min):
-                 m[h]+=min
+        m[i] = 0
+    df1 = df[df.Caller == x or df.Reciever == x]
+    for i in range(len(df1.index)):
+        h = int(df1.at[i,'Time'][0:2])
+        min = int(df1.at[i,'Time'][3:5])
+        dur = df1.at[i,'Duration']
+        min = 60-min
+        if(dur>min):
+             m[h]+=min
+        else:
+             m[h]+=dur
+        dur -= min
+        while(dur>0):
+            h = (h + 1)%24
+            if(dur>60):
+                m[h]+=60
+                dur-=60
             else:
-                 m[h]+=dur
-            dur -= min
-            while(dur>0):
-                h = (h + 1)%24
-                if(dur>60):
-                    m[h]+=60
-                    dur-=60
-                else:
-                    m[h]+=dur
-                    dur = 0
+                m[h]+=dur
+                dur = 0
     return (m)
 
 
@@ -66,11 +58,8 @@ def peakHours(x):
 #second element -> no. of incomming calls
 def ogIc(x):
     z = [0,0]
-    for i in range(rows_count):
-        if df.at[i,'Caller'] == x:
-            z[0]+=1
-        elif df.at[i,'Receiver'] == x:
-            z[1]+=1
+    z[0] = len(df[df.Caller == x])
+    z[1] = len(df[df.Reciever == x])
     return z
 
 #returns a list of size 3
@@ -81,20 +70,21 @@ def mostCalls(x):
     m1 = {}
     m2 = {}
     m3 = {}
-    for i in range(rows_count):
-        if df.at[i,'Caller'] == x:
-            if df.at[i,'Receiver'] in m1:
-                m1[df.at[i,'Receiver']]+=1
-                m3[df.at[i,'Receiver']]+=1
+    df1 = df[df.Caller == x or df.Reciever == x]
+    for i in range(len(df1.index)):
+        if df1.at[i,'Caller'] == x:
+            if df1.at[i,'Receiver'] in m1:
+                m1[df1.at[i,'Receiver']]+=1
+                m3[df1.at[i,'Receiver']]+=1
             else:
-                m1[df.at[i,'Receiver']]=1
-                m3[df.at[i,'Receiver']]=1
-        elif df.at[i,'Receiver'] == x:
-            if df.at[i,'Caller'] in m2:
-                m2[df.at[i,'Caller']]+=1
-                m3[df.at[i,'Caller']]+=1
+                m1[df1.at[i,'Receiver']]=1
+                m3[df1.at[i,'Receiver']]=1
+        elif df1.at[i,'Receiver'] == x:
+            if df1.at[i,'Caller'] in m2:
+                m2[df1.at[i,'Caller']]+=1
+                m3[df1.at[i,'Caller']]+=1
             else:
-                m2[df.at[i,'Caller']]=1
-                m3[df.at[i,'Caller']]=1
+                m2[df1.at[i,'Caller']]=1
+                m3[df1.at[i,'Caller']]=1
     z = [max(m1, key=m1.get),max(m2, key=m2.get),max(m3, key=m3.get)]
     return z
