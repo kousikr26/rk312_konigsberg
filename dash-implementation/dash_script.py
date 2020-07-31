b0ab637ccdd1aa1cd26d59f1c5a7c341fd07c780
kousikr26 <kousik18@iitg.ac.in>
2020-08-01 00:11:37 +0530

    cleanup

--- dash-implementation/dash_script.py
+++ dash-implementation/dash_script.py
@@ -28,12 +28,15 @@ external_stylesheets = [dbc.themes.SANDSTONE]
 
 # Load  Data
 df = pd.read_csv('./data/data.csv')
+df2 = pd.read_csv('./data/ipdr_data.csv')
 #### Create App ###
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.title = 'CDR/IPDR Analyser'
 #### Default Variables ####
 default_duration_slider_val = [0, 100]
-default_time_slider_val = ['00:00', '24:00']
+default_time_slider_val = [0,48]
+default_caller_receiver_val = 3
+
 date_format='%d-%m-%Y'
 # Loop to generate marks for Time
 time_str = ['0', '0', ':', '0', '0']
@@ -62,31 +65,72 @@ data_columns = ["Caller", "Receiver", "Date",
                 "Time", "Duration", "TowerID", "IMEI"]
 
 
+data_columns_ipdr = ["IMEI","Private IP","Private Port", "Public IP", "Public Port", "Dest IP","DEST PORT", "MSISDN", "IMSI", "Start Date","Start Time",\
+    "End Date","End Time", "CELL_ID", "Uplink Volume","Downlink Volume","Total Volume","I_RATTYPE"]
+
+
+ports_to_apps = {'DEST PORT':'App','5223':'WhatsApp','5228':'WhatsApp', '4244':'WhatsApp', '5222':'WhatsApp', '5242':'WhatsApp','443':'Skype',\
+    '3478-3481':'Skype','49152-65535':'Skype','80':'Web connection','8080':'Web Connection', \
+        '8081': 'Web Connection', '993':'IMAP', '143':'IMAP', '8024':'iTunes', '8027':'iTunes', '8013':'iTunes', \
+            '8017':'iTunes', '8003':'iTunes', '7275':'iTunes', '8025':'iTunes', '8009':'iTunes',\
+                '58128': 'Xsan', '51637':'Xsan', '61076':'Xsan','40020':'Microsoft Online Games', '40017':'Microsoft Various Games', \
+                    '40023':'Microsoft Online Games',\
+                    '40019':'Microsoft Online Games', '40001':'Microsoft Online Games', '40004':'Microsoft Online Games', \
+                        '40034':'Microsoft Online Games', '40031':'Microsoft Online Games', '40029':'Microsoft Online Games',\
+                              '40005':'Microsoft Online Games', '40026': 'Microsoft Online Games', '40008': 'Microsoft Online Games',\
+                                  '40032':'Microsoft Online Games'}   # did not take '443':'SSL, Web connection' to avoid double 443
+
 
 # Color scale of edges
 viridis = cm.get_cmap('viridis', 12)
-def preprocess_data(df):
+def preprocess_data(df,df2):
     # nodes
     nodes = np.union1d(df['Caller'].unique(), df['Receiver'].unique())
     # Define Color
     df['Dura_color'] = (df['Duration']/df['Duration'].max()).apply(viridis)
     df['Date'] = df['Date'].apply(lambda x:pd.to_datetime(x,format=date_format)).dt.date
 
-
     df['Caller_node'] = df['Caller'].apply(
         lambda x: list(nodes).index(x))  # Caller Nodes
+    
     df['Receiver_node'] = df['Receiver'].apply(lambda x: list(nodes).index(x))
+
+    df2['IMEI_node'] = df2['IMEI'].apply(lambda x: list(nodes).index(x))
+    df2['App_name'] = df2['DEST PORT'].apply(lambda x:ports_to_apps[str(x)])
+
     coords_to_node.clear()
     num_to_node.clear()
     node_to_num.clear()
 
-preprocess_data(df)
+preprocess_data(df,df2)
+
 #### Plots ####
-# Plot Graph of calls
+
 
 fig = go.Figure()
 pos = {}
 
+#Plot map 
+def plot_map(df):
+
+    people=dict(type='scattermapbox',lat=df['lat'],lon=df['lon'],mode='markers')
+    fig=go.Figure(people,layout={
+        'mapbox_style':'open-street-map',
+    
+        'mapbox':dict(
+        
+            bearing=0,
+            center=dict(
+                lat=23.2599,
+                lon=77.4126
+            ),
+            pitch=0,
+            zoom=10
+        )
+        
+    })
+    return fig
+# Plot Graph of calls
 def plot_network(df):
     G = nx.DiGraph()  # networkX Graph
     # Reciever Nodes
@@ -133,7 +177,6 @@ def plot_network(df):
         node_x.append(x)
         node_y.append(y)
         total_duration.append(15*pow((df[(df['Caller_node']==node)|(df['Receiver_node']==node)]['Duration'].sum())/df['Duration'].max(),0.3))
-    print(np.mean(np.array(total_duration)))
     node_trace = go.Scatter(
         x=node_x, y=node_y,
         mode='markers',
@@ -176,114 +219,116 @@ def plot_network(df):
 #### Callbacks ####
 # Callback to update df used for plotting
 app.layout = html.Div(children=[
-    html.Div(children=[
-        html.H1(children='CDR Analyser'),  # Title
-        html.H3(children='''
-        Analyse the phone calls between people.
-    '''),  # Subtitle
-        html.Div(
-            id='date-selected'
-        ),  # Date Selected Indicator
-        html.H4(id='message'),  # Message
-    ],id='header-text'),
+        html.Div(children=[
+            html.H1(children='CDR Analyser'),  # Title
+            html.H3(children='''
+            Analyse the phone calls between people.
+        '''),  # Subtitle
+            html.Div(
+                id='date-selected'
+            ),  # Date Selected Indicator
+            html.H4(id='message'),  # Message
+        ],id='header-text'),
 
     dbc.Row(children=[
-        dbc.Col(children=[
-            html.H3(
-                'Filters'
-            ),
-            html.H5(
-                'From:'
-            ),
-            dcc.DatePickerSingle(
-                id='date-picker1',
-                min_date_allowed=df['Date'].min(),
-                max_date_allowed=df['Date'].max(),
-                initial_visible_month=dt(2020, 6, 5),
-                date=str(dt(2020, 6, 17, 0, 0, 0)),
-                 display_format='DD-MMM-YY'
-            ),  # Data Picker
-            html.H5(
-                'To:'
-            ),
-            dcc.DatePickerSingle(
-                id='date-picker2',
-                min_date_allowed=df['Date'].min(),
-                max_date_allowed=df['Date'].max(),
-                initial_visible_month=dt(2020, 6, 5),
-                date=str(dt(2020, 6, 17, 0, 0, 0)),
-                display_format='DD-MMM-YY'
-            ),
-            dcc.RangeSlider(
-                id='duration-slider',
-                min=0,
-                max=df['Duration'].max(),
-                step=None,
-                marks=durations,
-
-                value=default_duration_slider_val,
-                dots=True,
-
-            ),  # Duration Slider
-
-            dcc.RangeSlider(
-                id='time-slider',
-                min=0,
-                max=48,
-                step=None,
-                marks=times,
-                dots=True,
-                value=[0, 48],
-                pushable=1
-
-            ),  # Time Slider
-            html.H5(
-                'Condition for Caller/Reciever'
-            ),
-            dcc.Dropdown(
-                id='select-caller-receiver',
-                options=[{'label': 'Only Caller', 'value': 1}]+[{'label': 'Only Receiver', 'value': 2}]+[
-                    {'label': 'Either Caller or Reciever', 'value': 3}]+[{'label': 'Both Caller and Reciever', 'value': 4}],
-                value=3,
-            ),  # Select if you want the select the numbers to be from Caller/Reciever/Both/Either
-           
-            html.H5(
-                'Select Caller:'
-            ),
-            dcc.Dropdown(
-                id='caller-dropdown',
-                options=[{'label': 'None', 'value': 'None'}] + \
-                [{'label': k, 'value': k} for k in df['Caller'].unique()],
-                value='None',
-                multi=True,
-            ),  # Dropdown for Caller
-            html.H5(
-                'Select Reciever:'
-            ),
-            dcc.Dropdown(
-                id='receiver-dropdown',
-                options=[{'label': 'None', 'value': 'None'}] + \
-                [{'label': k, 'value': k} for k in df['Receiver'].unique()],
-                value='None',
-                multi=True,
-            ),  # Dropdown for Reciever,
-           
-           
-            html.Div([
-                dcc.Upload(
-                    id='upload-data',
-                    children=html.Div([
-                        'Drag and Drop or ',
-                        html.A('Select a File')
-                    ]),
-                    
-                    # Allow multiple files to be uploaded
-                    multiple=False
+            dbc.Col(children=[
+                html.H3(
+                    'Filters'
                 ),
-                html.Div(id='output-data-upload'),
-            ])
-            ],
-            id='filters',lg=3),  # Filters
+                html.H5(
+                    'From:'
+                ),
+                dcc.DatePickerSingle(
+                    id='date-picker1',
+                    min_date_allowed=df['Date'].min(),
+                    max_date_allowed=df['Date'].max(),
+                    initial_visible_month=dt(2020, 6, 5),
+                    date=str(dt(2020, 6, 17, 0, 0, 0)),
+                    display_format='DD-MMM-YY'
+                ),  # Data Picker
+                html.H5(
+                    'To:'
+                ),
+                dcc.DatePickerSingle(
+                    id='date-picker2',
+                    min_date_allowed=df['Date'].min(),
+                    max_date_allowed=df['Date'].max(),
+                    initial_visible_month=dt(2020, 6, 5),
+                    date=str(dt(2020, 6, 17, 0, 0, 0)),
+                    display_format='DD-MMM-YY'
+                ),
+                dcc.RangeSlider(
+                    id='duration-slider',
+                    min=0,
+                    max=df['Duration'].max(),
+                    step=None,
+                    marks=durations,
+
+                    value=default_duration_slider_val,
+                    dots=True,
+
+                ),  # Duration Slider
+
+                dcc.RangeSlider(
+                    id='time-slider',
+                    min=0,
+                    max=48,
+                    step=None,
+                    marks=times,
+                    dots=True,
+                    value=[0, 48],
+                    pushable=1
+
+                ),  # Time Slider
+                html.H5(
+                    'Condition for Caller/Reciever'
+                ),
+                dcc.Dropdown(
+                    id='select-caller-receiver',
+                    options=[{'label': 'Only Caller', 'value': 1}]+[{'label': 'Only Receiver', 'value': 2}]+[
+                        {'label': 'Either Caller or Reciever', 'value': 3}]+[{'label': 'Both Caller and Reciever', 'value': 4}],
+                    value=3,
+                ),  # Select if you want the select the numbers to be from Caller/Reciever/Both/Either
+            
+                html.H5(
+                    'Select Caller:'
+                ),
+                dcc.Dropdown(
+                    id='caller-dropdown',
+                    options=[{'label': 'None', 'value': 'None'}] + \
+                    [{'label': k, 'value': k} for k in df['Caller'].unique()],
+                    value='None',
+                    multi=True,
+                ),  # Dropdown for Caller
+                html.H5(
+                    'Select Reciever:'
+                ),
+                dcc.Dropdown(
+                    id='receiver-dropdown',
+                    options=[{'label': 'None', 'value': 'None'}] + \
+                    [{'label': k, 'value': k} for k in df['Receiver'].unique()],
+                    value='None',
+                    multi=True,
+                ),  # Dropdown for Reciever,
+                
+                
+                
+                html.Div([
+                    dcc.Upload(
+                        id='upload-data',
+                        children=html.Div([
+                            'Drag and Drop or ',
+                            html.A('Select a File')
+                        ]),
+                        
+                        # Allow multiple files to be uploaded
+                        multiple=False
+                    ),
+                    html.Div(id='output-data-upload'),
+                ]),
+                html.Button('Reset Filters', id='reset-button', n_clicks=0)
+                ],
+                id='filters',lg=3),  # Filters
 
         dbc.Col(
            [ html.H3('Network Graph : '),
@@ -291,7 +336,13 @@ app.layout = html.Div(children=[
                 id='network-plot'
 
             ),
-            html.H5('The size of the dots denote the total duration of the caller/callee')],id='plot-area',lg=6),     # Network Plot
+            html.H5('The size of the dots denote the total duration of the caller/callee'),
+            dcc.Markdown("""
+             		1. The Duration the selected person has spent on exchanging calls over time
+            		"""),
+         	dcc.Graph(id='duration-plot'),
+            ],id='plot-area',lg=6),     # Network Plot
+            
         dbc.Col(children=[
             html.H3('Statistics'),
             html.Div([
@@ -309,6 +360,9 @@ app.layout = html.Div(children=[
                 Click on points in the graph to get the call data records.
             """),
                 html.Pre(id='click-data', ),
+                dcc.Graph(
+                    id='pie-chart'
+                )
             ], ),  # Click Data Container
 
             html.Div([
@@ -398,7 +452,7 @@ def update_filtered_div_caller(contents, selected_date1, selected_date2, selecte
 def display_hover_data(hoverData, filtered_data):
 
     df = pd.read_json(filtered_data, orient='split')
-    if hoverData is not None:
+    if hoverData is not None and 'marker.size' in hoverData['points'][0]:
         # Get node number corresponding to the point.
 
         nodeNumber = coords_to_node[(
@@ -427,15 +481,36 @@ def display_hover_data(hoverData, filtered_data):
 
 
 @app.callback(
-    Output('click-data', 'children'), #Suggest to put all extra plots in this callback's output...
+    [Output('click-data', 'children'), Output('pie-chart','figure'), Output('duration-plot','figure')], #Suggest to put all extra plots in this callback's output...
     [Input('network-plot', 'clickData')])
 def display_click_data(clickData):
-    if clickData is not None:
+    if clickData is not None and 'marker.size' in clickData['points'][0]:
         nodeNumber = coords_to_node[(
             clickData['points'][0]['x'], clickData['points'][0]['y'])]
+        groups=df2[df2['IMEI_node']==nodeNumber].groupby('App_name')['IMEI'].count()
+        print(groups,nodeNumber)
+        fig = go.Figure(data=dict(type='pie',values=groups,labels=groups.index))
+        fig.update_layout(showlegend=False)
+        print(fig)
         # Filtering DF
-        return df[(df['Caller_node'] == nodeNumber) | (df['Receiver_node'] == nodeNumber)][data_columns].to_string(index=False)
-    return "Click on a node to view more data"
+        new_df = df[(df['Caller_node'] == nodeNumber) | (df['Receiver_node'] == nodeNumber)][data_columns]
+
+        return df[(df['Caller_node'] == nodeNumber) | (df['Receiver_node'] == nodeNumber)][data_columns].to_string(index=False), fig, plot_Duration(new_df)
+    return "Click on a node to view more data",go.Figure(),go.Figure() #DO NOT RETURN HERE 'None', otherwise duration-plot will always be empty.
+#Callback to output the new figure for Duration plot of selected node.
+def plot_Duration(new_df):
+
+	if new_df is not None:
+
+		x = sorted(new_df["Date"])
+		y = new_df["Duration"]
+		fig = go.Figure([ go.Scatter(x = x, y = y,
+	                           mode='lines+markers',
+	                            name='plot') ])  
+		return fig
+	else:
+		return None
+
 
 components = []
 # Callback for Selected Data
@@ -520,6 +595,13 @@ def update_phone_div_caller(selected_date1, selected_date2):
 def update_phone_div_receiver(selected_date1, selected_date2):
     return [{'label': 'None', 'value': ''}]+[{'label': k, 'value': k} for k in df[(df['Date'] >= pd.to_datetime(selected_date1)) & (df['Date']<=pd.to_datetime(selected_date2))]['Receiver'].unique()]
 
+# Reset Filters
+@app.callback(
+	[Output('date-picker1', 'date'), Output('date-picker2', 'date'), Output('duration-slider', 'value'), Output('time-slider', 'value'), Output('select-caller-receiver', 'value')],
+	[Input('reset-button', 'n_clicks')]
+	)
+def ResetFilters(button_reset):
+	return str(dt(2020, 6, 17, 0, 0, 0)), str(dt(2020, 6, 17, 0, 0, 0)), default_duration_slider_val, default_time_slider_val, default_caller_receiver_val
 
 #### Run Server ####
 if __name__ == '__main__':
